8923f47718eeba3c8aaee697a2163778b49964c2
shandell martell <emmafeathershandell@gmail.com>
2025-05-07 19:41:17 -0500

    tried

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,9 @@
 window.onload = ()=>{
     document.querySelector("#shandell").addEventListener("click", setImgtoshandell);
     document.querySelector("#shandell2").addEventListener("click", setImgtoshandell2);
-    
+    document.querySelector("expected").addEventListener("hover", mouseover);
+    document.querySelector("wounded").addEventListener("hover" , mouseover);
+    document.querySelector("eagle").addEventListener("hover" , mouseover);
    
 }
 
@@ -13,4 +15,9 @@ const setImgtoshandell = () =>{
 const setImgtoshandell2 = () =>{
     const img = document.querySelector('#attributes-img');
     img.src = "pictures/shandell2.jpg";
+}
+
+const mouseover = () =>{
+    const div = document.querySelector('hover');
+    div.style.mouseover = (div.style.mouseover == 'rgb(153, 66, 127)') ? 'pink' : 'rgb(153 66, 127)';
 }
